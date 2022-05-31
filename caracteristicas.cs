c7664b4266fcc3edc1379c4ff3560e7271541140
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2022-05-31 04:00:41 -0300

    creo clases

---
+++ caracteristicas.cs
@@ -0,0 +1,20 @@
+namespace rpg_2022_exequiel1984
+{
+    public class caracteristicas {
+        private int velocidad;
+        private int destreza;
+        private int fuerza;
+        private int nivel;
+        private int armadura;
+
+        public int Velocidad { get => velocidad; set => velocidad = value; }
+        public int Destreza { get => destreza; set => destreza = value; }
+        public int Fuerza { get => fuerza; set => fuerza = value; }
+        public int Nivel { get => nivel; set => nivel = value; }
+        public int Armadura { get => armadura; set => armadura = value; }
+
+        public caracteristicas(){
+            
+        }
+    }
+}
---
+++ personaje.cs
@@ -0,0 +1,5 @@
+namespace rpg_2022_exequiel1984
+{
+    public class personaje {
+    }
+}
