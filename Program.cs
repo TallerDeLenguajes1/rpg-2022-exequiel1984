16555f0cba3f6cc8d984970d15050c5e46f28134
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2022-06-10 09:26:35 -0300

    ya carga datos del personaje

--- Program.cs
+++ Program.cs
@@ -1,9 +1,26 @@
 ﻿using System.Collections.Generic;
 using System.Text.Json;
 using System.IO;
+using System;
 
 using rpg_2022_exequiel1984;
 
-var ListadoPersonajes = new List<personaje>();
+var ListadoPersonajes = new List<Personaje>();
 
-CrearPersonaje();
+var NuevoPersonaje = new Personaje(); 
+CargarDatos(NuevoPersonaje);
+string salida = NuevoPersonaje.Edad().ToString();
+System.Console.WriteLine("Edad = " + salida);
+
+
+void CargarDatos(Personaje personaje)
+{            
+        Random rand = new Random();
+
+        int IndexTipo = rand.Next(0, Enum.GetValues(typeof(tipos)).Length);
+        personaje.Tipo = Enum.GetName(typeof(tipos), IndexTipo);
+        int IndexNombre = rand.Next(0, Enum.GetValues(typeof(nombres)).Length);
+        personaje.Nombre = Enum.GetName(typeof(nombres), IndexNombre);
+        personaje.FechaNacimiento = new DateTime(rand.Next(1722, 2022), rand.Next(01, 13), rand.Next(01, 32));            
+        personaje.Salud = 100;
+}
--- personaje.cs
+++ personaje.cs
@@ -2,26 +2,50 @@ using System.Collections.Generic;
 using System.Text.Json;
 using System.IO;
 
-using espacioDatos;
+using rpg_2022_exequiel1984;
 
 namespace rpg_2022_exequiel1984
 {
-    public class personaje {
-        private Caracteristicas caracteristicas; 
-        private Datos datos;
+    public class Personaje {
+        
+        private string tipo;
+        private string nombre;
+        private string apodo;
+        private DateTime fechaNacimiento;        
+        private int salud;
 
-        public Caracteristicas Caracteristicas { get => caracteristicas; set => caracteristicas = value; }
-        public Datos Datos { get => datos; set => datos = value; }
+        private int velocidad;
+        private int destreza;
+        private int fuerza;
+        private int nivel;
+        private int armadura;
 
-        public personaje(){
-            this.Caracteristicas = new Caracteristicas();
-            this.Datos = new Datos();
+        public int Velocidad { get => velocidad; set => velocidad = value; }
+        public int Destreza { get => destreza; set => destreza = value; }
+        public int Fuerza { get => fuerza; set => fuerza = value; }
+        public int Nivel { get => nivel; set => nivel = value; }        
+
+        public string Tipo { get => tipo; set => tipo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apodo { get => apodo; set => apodo = value; }
+        public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public int Salud { get => salud; set => salud = value; }        
+
+        public Personaje()
+        {
+        
         }
 
+        public int Edad()
+        {
+            return DateTime.Now.Year - FechaNacimiento.Year;
+        }
+        /*
         static void CrearPersonaje(){
             var NuevoPersonaje = new personaje();
             NuevoPersonaje.Datos.CargarDatos();
             System.Console.WriteLine(NuevoPersonaje.Datos.Salud);
-        }       
+        } 
+        */      
     }
 }
